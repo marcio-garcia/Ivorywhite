72fb65d5c0af93becb7bd5b072e0e2788e11c01a
Marcio Garcia <marcio.garcia@willbank.com.br>
2021-04-14 19:12:36 -0300

    Add more error objects

--- Sources/Ivorywhite/Service/Service.swift
+++ Sources/Ivorywhite/Service/Service.swift
@@ -8,32 +8,6 @@
 
 import Foundation
 
-public struct NetworkError: LocalizedError {
-    /// A localized message describing what error occurred.
-    public var errorDescription: String?
-    /// A localized message describing the reason for the failure.
-    public var failureReason: String?
-    /// A localized message describing how one might recover from the failure.
-    public var recoverySuggestion: String?
-    /// A localized message providing "help" text if the user requests help.
-    public var helpAnchor: String?
-
-    static func invalidRequest(request: NetworkRequest) -> NetworkError {
-        let errorDescription = "Ivorywhite: Invalid request!"
-        let baseURL = String(describing: request.baseURL)
-        let httpHeaders = String(describing: request.httpHeaders)
-        let parameters = String(describing: request.parameters)
-        let encoding = String(describing: request.encoding)
-        let failureReason = "Ivorywhite: could not build the request with the following data - httpMethod=\(request.httpMethod); baseURL=\(baseURL); path=\(request.path) httpHeaders=\(httpHeaders); parameters=\(parameters); encoding=\(encoding)"
-        return NetworkError(errorDescription: errorDescription, failureReason: failureReason, recoverySuggestion: nil, helpAnchor: nil)
-    }
-
-    static func unableToDecode(data: Data) -> NetworkError {
-        let string = String(describing: String(data: data, encoding: .utf8))
-        return NetworkError(errorDescription: "Ivorywhite: Unable to decode response.", failureReason: "Ivorywhite: the parser implementation provided by the caller was not able to decode the data returned in the response body. Printing the string representation of the response body: \(string)", recoverySuggestion: nil, helpAnchor: nil)
-    }
-}
-
 class Service: NetworkService {
 
     private var tasks = NSCache<NSString, URLSessionTask>()
@@ -111,13 +85,13 @@ class Service: NetworkService {
 
             guard let resp = response as? HTTPURLResponse else {
                 let response = ResponseData(statusCode: 500,
-                                            result: .failure(NetworkError(errorDescription: "Ivorywhite: Invalid response!")))
+                                            result: .failure(NetworkError.invalidResponse(response: response)))
                 completion(response)
                 return
             }
 
             if resp.statusCode > 299 {
-                let response = ResponseData(statusCode: resp.statusCode, result: .failure(NetworkError(errorDescription: "Ivorywhite: Network error.")))
+                let response = ResponseData(statusCode: resp.statusCode, result: .failure(NetworkError.networkError(statusCode: resp.statusCode)))
                 completion(response)
                 return
             }
@@ -143,17 +117,17 @@ class Service: NetworkService {
                                 errorModel: ErrorResponseModel.Type) -> Response {
         guard let resp = response as? HTTPURLResponse else {
             return Response(statusCode: 500,
-                            result: .failure(NetworkError(errorDescription: "Ivorywhite: Invalid response!")))
+                            result: .failure(NetworkError.invalidResponse(response: response)))
         }
 
         if resp.statusCode > 299 {
             guard let errorData = data else {
-                return Response(statusCode: resp.statusCode, result: .failure(NetworkError(errorDescription: "Ivorywhite: Network error.")))
+                return Response(statusCode: resp.statusCode, result: .failure(NetworkError.networkError(statusCode: resp.statusCode)))
             }
 
             guard let parsedErrorData = errorModel.parse(data: errorData) else {
                 return Response(statusCode: resp.statusCode,
-                                result: .failure(NetworkError(errorDescription: "Ivorywhite: Unable to decode response.")))
+                                result: .failure(NetworkError.unableToDecode(data: errorData)))
             }
 
             return Response(statusCode: resp.statusCode, result: .failure(parsedErrorData))
